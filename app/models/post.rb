c32199f858a4f1a9ada60c770d6b2cc9dd3b276c
Finn Dethleff <fdethleff@gmail.com>
2022-12-17 13:37:02 +0100

    Add basic feature version and seed db

---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+  # Validate inputs to prevent bad data from being saved to the database
+  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :body, length: { maximum: 1000 }
+end
